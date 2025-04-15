da184314b336f1d0ef8c63bab2e528fd9bd2ae2a
Isaque <isaqueestolanossi04@gmail.com>
2025-04-15 15:00:04 -0300

    Add interactive study box and update styles for improved user experience

---
+++ script/index.js
@@ -0,0 +1,16 @@
+function add() {
+  var addcross = `<div id="add" onclick="add()">
+        <h2>+</h2>
+      </div>`;
+  var mother = document.getElementById("mother-box");
+  var html = `
+    <div id="study2" class="studybox">
+        <h2>TESTE</h2>
+        <p>Link</p>
+    </div>`;
+  var boxes = document.getElementsByClassName("studybox");
+  var boxesHTML = Array.from(boxes)
+    .map((box) => box.outerHTML)
+    .join("");
+  mother.innerHTML = boxesHTML + html + addcross;
+}
